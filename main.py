91862a28c740cb1293ee88ac59ee9a394a170e04
djsloat <43764827+djsloat@users.noreply.github.com>
2023-03-24 21:46:37 -0700

    Refactor

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-"""Main document processing script"""
+"""Document processing script"""
 
 from pathlib import Path
-import tomllib
 
-from comment_extract.comment_record import CommentRecord
+import tomllib
 from docx_comments import Document
+
+from comment_extract import CommentRecord
 from comment_extract.logger import logger_init
 
 
@@ -15,8 +16,9 @@ def docx_in_folder(folder_path: str):
 
 @logger_init()
 def main():
-    with open("config.toml", "rb") as f:
-        config = tomllib.load(f)
+    with open("config.toml", "rb") as toml:
+        config = tomllib.load(toml)
+
     comment_record = CommentRecord()
     for document in docx_in_folder(config["folder_path"]):
         doc = Document(document)
